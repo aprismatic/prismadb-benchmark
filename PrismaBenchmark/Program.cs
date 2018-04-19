f9bbbc7cfc0b76c2b52a5059e45f366576472fa2
helloWorld141 <nghia002@outlook.com>
2018-04-19 10:09:34 +0800

    Add project files.

---
+++ PrismaBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrismaBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
