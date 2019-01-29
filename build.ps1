d8455ac89375722bb3f7d930ed8cbd8ab4291326
Baron-Chen <baronchen1350@gmail.com>
2019-01-29 13:53:58 +0800

    Add the VM creation script.

--- build.ps1
+++ build.ps1
@@ -1,10 +1,22 @@
 Push-Location $PSScriptRoot
 
 try {
-  docker-compose up -d --build prismadb prismaproxy
-  docker-compose up --build prismabenchmark
+
+	$DockerMachine = "BenchmarkTest"
+
+	docker-machine create --driver digitalocean --digitalocean-access-token $DigitalOceanToken `
+						--digitalocean-region sgp1 --digitalocean-size s-4vcpu-8gb $DockerMachine
+
+	docker-machine env $DockerMachine | Invoke-Expression
+
+	docker-compose up -d --build prismadb prismaproxy
+	docker-compose up --build prismabenchmark
+	
+	docker-machine stop $DockerMachine
+	echo "y" | docker-machine rm $DockerMachine
+	
 }
 finally {
-  if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
-  Pop-Location
+	if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+	Pop-Location
 }
