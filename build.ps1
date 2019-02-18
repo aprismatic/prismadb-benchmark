22451a9f23157c0ac4379612cb673ef1e0fe9c1b
Baron-Chen <baronchen1350@gmail.com>
2019-02-18 15:58:35 +0800

    Block after create docker machine

--- build.ps1
+++ build.ps1
@@ -8,6 +8,9 @@ try {
 	docker-machine create --driver digitalocean --digitalocean-access-token $DigitalOceanToken `
 						--digitalocean-region='sgp1' --digitalocean-size='s-4vcpu-8gb' $DockerMachine
 
+	$blockRdp = $true
+	iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+	
 	docker-machine env $DockerMachine | Invoke-Expression
 
 	docker-compose up -d --build prismadb prismaproxy
