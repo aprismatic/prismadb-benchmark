3789099c385289c008728108d0f550e59fa4165e
Baron-Chen <baronchen1350@gmail.com>
2019-02-18 16:21:14 +0800

    Block after trying to connect to remote docker machine

--- build.ps1
+++ build.ps1
@@ -8,11 +8,12 @@ try {
 	docker-machine create --driver digitalocean --digitalocean-access-token $DigitalOceanToken `
 						--digitalocean-region='sgp1' --digitalocean-size='s-4vcpu-8gb' $DockerMachine
 
+	docker-machine env --shell powershell $DockerMachine
+	docker-machine env --shell=powershell $DockerMachine | Invoke-Expression
+
 	$blockRdp = $true
 	iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 	
-	docker-machine env $DockerMachine | Invoke-Expression
-
 	docker-compose up -d --build prismadb prismaproxy
 	docker-compose up --build prismabenchmark
 	
