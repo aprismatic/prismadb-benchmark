4e8d23d93329180fe8b4d9a40a66f34657dd4f34
Baron-Chen <baronchen1350@gmail.com>
2019-02-18 17:02:05 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -6,11 +6,11 @@ try {
 	$DockerMachine = 'BenchmarkTest'
 
 	docker-machine create --driver digitalocean --digitalocean-access-token $DigitalOceanToken `
-						--digitalocean-region='sgp1' --digitalocean-size='s-4vcpu-8gb' $DockerMachine
+						--digitalocean-region='sgp1' --digitalocean-size='s-1vcpu-1gb' $DockerMachine
 
 						
-	C:\Program Files\Docker\Docker\Resources\bin\docker-machine.exe env --shell powershell $DockerMachine
-	C:\Program Files\Docker\Docker\Resources\bin\docker-machine.exe env --shell powershell $DockerMachine | Invoke-Expression
+	& 'C:\Program Files\Docker\Docker\Resources\bin\docker-machine.exe' env --shell powershell $DockerMachine
+	& 'C:\Program Files\Docker\Docker\Resources\bin\docker-machine.exe' env --shell powershell $DockerMachine | Invoke-Expression
 
 	$blockRdp = $true
 	iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
