1ec322bddb3a14be47bacd6b2c7c18fd2ea64740
Baron-Chen <baronchen1350@gmail.com>
2019-02-18 16:49:16 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -8,14 +8,13 @@ try {
 	docker-machine create --driver digitalocean --digitalocean-access-token $DigitalOceanToken `
 						--digitalocean-region='sgp1' --digitalocean-size='s-4vcpu-8gb' $DockerMachine
 
-	docker-machine env --shell powershell $DockerMachine
 	docker-machine env --shell powershell $DockerMachine | Invoke-Expression
-
-	$blockRdp = $true
-	iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 	
 	docker-compose up -d --build prismadb prismaproxy
 	docker-compose up --build prismabenchmark
+	
+	$blockRdp = $true
+	iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 	
 	docker-machine stop $DockerMachine
 	echo "y" | docker-machine rm $DockerMachine
